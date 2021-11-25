6637ba7564b6b74931f55fb0f9aef5ba5299e610
JulesGranget <jules.granget@gmail.com>
2021-11-25 15:03:04 +0100

    enable script execute on nodeGPU

--- n9_big_execute.py
+++ n9_big_execute.py
@@ -8,17 +8,10 @@ from n0_config import *
 
 start = time.time()
 
-os.chdir(path_main_workdir)
-import n5_precompute_surrogates
-
-os.chdir(path_main_workdir)
-import n6_precompute_TF
-
-os.chdir(path_main_workdir)
-import n7_power_analysis
-
-os.chdir(path_main_workdir)
-import n8_fc_analysis
+os.system('python3 n5_precompute_surrogates.py')
+os.system('python3 n6_precompute_TF.py')
+os.system('python3 n7_power_analysis')
+os.system('python3 n8_fc_analysis')
 
 print('######## COMPUTING TIME ########')
 print(time.time()-start, 'seconds')
